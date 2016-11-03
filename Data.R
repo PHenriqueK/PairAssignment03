9dadcbb5c262b8853d51e6af45ddcb87abd985ca
Dan Murphy <danieljmurphy01@gmail.com>
2016-11-03 11:04:00 -0400

    First airbnb import

--- Data.R
+++ Data.R
@@ -27,3 +27,7 @@ library(rio)
 #Loading Data from "Regionaldatenbank Deutschland"
 
 test01 <- import('https://www.regionalstatistik.de/genesis/online/data/Tabelle01.csv;jsessionid=0E492BFA5C08791DB463EF5123E0D36C?operation=ergebnistabelleDownload&levelindex=2&levelid=1478182953986&option=csv&doDownload=csv&contenttype=csv', format = 'csv')
+
+
+#Import "listings.csv" from insideairbnb.com
+Airbnb_Listings <- read.csv("http://data.insideairbnb.com/germany/be/berlin/2015-10-03/visualisations/listings.csv")
