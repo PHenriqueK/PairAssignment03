16c02e859642fe48c4e82df2c5e7a9c211403b32
Paulo Kalkhake <pkalkhake@gmail.com>
2016-11-03 17:06:54 +0100

    data

--- Data.R
+++ Data.R
@@ -23,9 +23,22 @@ library(pastecs)
 library(Hmisc)
 library(curl)
 library(rio)
+library(rvest)
+library(stringr)
+library(plyr)
+library(dplyr)
+library(ggvis)
+library(knitr)
+library(curl)
+library(xml2)
+library(httr)
+library(rvest)
+library(stringi)
+library(rPython)
 
 #Loading Data from "Regionaldatenbank Deutschland"
 
+<<<<<<< Updated upstream
 test01 <- import('https://www.regionalstatistik.de/genesis/online/data/Tabelle01.csv;jsessionid=0E492BFA5C08791DB463EF5123E0D36C?operation=ergebnistabelleDownload&levelindex=2&levelid=1478182953986&option=csv&doDownload=csv&contenttype=csv', format = 'csv')
 
 
@@ -50,3 +63,11 @@ Reviews_URL <- "http://data.insideairbnb.com/germany/be/berlin/2015-10-03/data/r
 download.file(Reviews_URL, temp)
 Detailed_Reviews <- read.csv(gzfile(temp, "reviews.csv"))
 unlink(temp)
+=======
+# warm up the curl handle
+start <- GET("https://www.statistik-berlin-brandenburg.de/webapi/jsf/tableView/tableView.xhtml")
+
+# get the cookies
+ck <- handle_cookies(handle_find("https://www.statistik-berlin-brandenburg.de/webapi/jsf/tableView/tableView.xhtml")$handle)
+
+>>>>>>> Stashed changes
