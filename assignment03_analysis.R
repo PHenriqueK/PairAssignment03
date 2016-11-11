6b1f04551db88e95ee38c9f3872968035a3818cf
Paulo Kalkhake <pkalkhake@gmail.com>
2016-11-11 18:46:39 +0100

    more stuff

--- assignment03_analysis.R
+++ assignment03_analysis.R
@@ -10,8 +10,6 @@
 # Set working directory
 try(setwd("/Users/Paulo/GitHub/PairAssignment03"),silent=TRUE)
 try(setwd("/Users/djm113/Documents/GitHub/PairAssignment03/"),silent=TRUE)
-getwd()
-
 
 #dynamical link
 source('merge.R')
@@ -26,7 +24,7 @@ data_2010_2014$apt_new[is.na(data_2010_2014$apt_new)] <- 0
 data_2010_2014$AB_supply <- ave(data_2010_2014$apt_new, data_2010_2014$NID, FUN=cumsum)
 
 #dropping variables not needed
-analysis_simple <- data_2010_2014[, c("NID", "neighbourhood", "year_month", "occup_rate", "AB_supply", "avg_inc", "ue_rate", "guests" )]
+analysis_simple <- data_2010_2014[, c("NID", "neighbourhood", "year_month", "year", "occup_rate", "AB_supply", "avg_inc", "ue_rate", "guests" )]
 
 #log Airbnb Apt Supply (simple model)
 analysis_simple$log_ABsupply <- log(analysis_simple$AB_supply)
